4db876c7cdfe0a7305a1ef9738684989592f6030
mesier-stack <dantito.cortes.prado@gmail.com>
2026-08-20 16:07:52 -0400

    Load active-window awareness in ULTRON

--- ultron_entry.py
+++ ultron_entry.py
@@ -2,11 +2,13 @@ from ultron_identity import install_identity_patch
 from ultron_vision import install_vision_patch
 from ultron_health import install_health_patch
 from ultron_focus import install_focus_patch
+from ultron_awareness import install_awareness_patch
 
 install_identity_patch()
 install_vision_patch()
 install_health_patch()
 install_focus_patch()
+install_awareness_patch()
 
 import runpy
 
