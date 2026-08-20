5e54abeab814af938991386dea9f56f7ce97b4f2
mesier-stack <dantito.cortes.prado@gmail.com>
2026-08-20 16:11:54 -0400

    Load bilingual ULTRON speech layer

--- ultron_entry.py
+++ ultron_entry.py
@@ -3,12 +3,14 @@ from ultron_vision import install_vision_patch
 from ultron_health import install_health_patch
 from ultron_focus import install_focus_patch
 from ultron_awareness import install_awareness_patch
+from ultron_bilingual import install_bilingual_patch
 
 install_identity_patch()
 install_vision_patch()
 install_health_patch()
 install_focus_patch()
 install_awareness_patch()
+install_bilingual_patch()
 
 import runpy
 
