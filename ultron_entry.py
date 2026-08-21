8c0ce79a9e58ad6f968ad474cb2b4e64e1d23483
mesier-stack <dantito.cortes.prado@gmail.com>
2026-08-21 08:14:15 -0400

    Load ULTRON history and command center

--- ultron_entry.py
+++ ultron_entry.py
@@ -10,6 +10,7 @@ from ultron_windows import install_window_control_patch
 from ultron_permissions import install_permission_patch
 from ultron_memory_categories import install_memory_category_patch
 from ultron_updater import install_update_check_patch
+from ultron_action_history import install_action_history_patch
 
 install_identity_patch()
 install_vision_patch()
@@ -23,17 +24,20 @@ install_window_control_patch()
 install_permission_patch()
 install_memory_category_patch()
 install_update_check_patch()
+install_action_history_patch()
 
 import ultron_main
 from ultron_brain_ui import install_brain_ui
 from ultron_overlay import install_overlay
 from ultron_watch import install_watch_mode
 from ultron_conversation import install_conversation_mode
+from ultron_command_center import install_command_center
 
 install_brain_ui(ultron_main.UltronApp)
 install_overlay(ultron_main.UltronApp)
 install_watch_mode(ultron_main.UltronApp)
 install_conversation_mode(ultron_main.UltronApp)
+install_command_center(ultron_main.UltronApp)
 
 if __name__ == "__main__":
     app = ultron_main.UltronApp()
